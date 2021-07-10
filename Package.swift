c783d677f65540c2c68c43023293df13c437c83f
Felix Desiderato <felix.desiderato@tum.de>
2021-07-10 19:09:01 +0200

    update package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "DeviceDiscovery",
+    name: "swift-device-discovery",
     platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DeviceDiscovery",
+            name: "SwiftDeviceDiscovery",
             targets: ["DeviceDiscovery"])
     ],
     dependencies: [
