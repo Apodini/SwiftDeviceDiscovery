52d2f2ca7e354710eaa53f427f75df09b3040391
Felix Desiderato <felix.desiderato@tum.de>
2021-07-21 14:03:23 +0200

    Removed unused code from InteractivePasswordPromptDelegate

--- Sources/DeviceDiscovery/SSH/InteractivePasswordPromptDelegate.swift
+++ Sources/DeviceDiscovery/SSH/InteractivePasswordPromptDelegate.swift
@@ -11,18 +11,9 @@ import NIO
 import NIOSSH
 
 /// A client user auth delegate that provides an interactive prompt for password-based user auth.
-public class InteractivePasswordPromptDelegate: NIOSSHClientUserAuthenticationDelegate {
-    private let queue: DispatchQueue
-
-    private var username: String?
-
-    private var password: String?
-
-    public init(username: String?, password: String?) {
-        self.queue = DispatchQueue(label: "io.swiftnio.ssh.InteractivePasswordPromptDelegate")
-        self.username = username
-        self.password = password
-    }
+public struct InteractivePasswordPromptDelegate: NIOSSHClientUserAuthenticationDelegate {
+    let username: String
+    let password: String
 
     public func nextAuthenticationType(availableMethods: NIOSSHAvailableUserAuthenticationMethods, nextChallengePromise: EventLoopPromise<NIOSSHUserAuthenticationOffer?>) {
         guard availableMethods.contains(.password) else {
@@ -30,24 +21,7 @@ public class InteractivePasswordPromptDelegate: NIOSSHClientUserAuthenticationDe
             nextChallengePromise.fail(SSHClientError.passwordAuthenticationNotSupported)
             return
         }
-
-        self.queue.async {
-            if self.username == nil {
-                print("Username: ", terminator: "")
-                self.username = readLine() ?? ""
-            }
-
-            if self.password == nil {
-                #if os(Windows)
-                print("Password: ", terminator: "")
-                self.password = readLine() ?? ""
-                #else
-                self.password = String(cString: getpass("Password: "))
-                #endif
-            }
-
-            nextChallengePromise.succeed(NIOSSHUserAuthenticationOffer(username: self.username!, serviceName: "", offer: .password(.init(password: self.password!))))
-        }
+        nextChallengePromise.succeed(NIOSSHUserAuthenticationOffer(username: self.username, serviceName: "", offer: .password(.init(password: self.password))))
     }
 }
 
