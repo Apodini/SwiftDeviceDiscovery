0ec35799b72b04650e0dbeb18a88a90d0a83fc31
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-04-15 16:17:30 +0200

    Update GitHub Actions

--- Package.swift
+++ Package.swift
@@ -11,25 +11,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-device-discovery",
+    name: "SwiftDeviceDiscovery",
     platforms: [.macOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftDeviceDiscovery",
-            targets: ["DeviceDiscovery"]),
+            targets: ["DeviceDiscovery"]
+        ),
         .executable(name: "discovery-executable", targets: ["discovery-executable"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
         .package(url: "https://github.com/apple/swift-nio-ssh", from: "0.3.0"),
         .package(url: "https://github.com/Bouke/NetService.git", from: "0.8.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DeviceDiscovery",
             dependencies: [
